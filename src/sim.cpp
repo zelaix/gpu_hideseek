864911357c58b9be6d1a2bf37e0a0a3eb06fdf27
zelaix <zelai.eecs@gmail.com>
2023-11-13 23:33:21 +0800

    fix reward bug.

--- src/sim.cpp
+++ src/sim.cpp
@@ -799,7 +799,7 @@ inline void rewardsVisSystem(Engine &ctx,
         if (hit_entity == hider_sim_e && to_hider_dist <= max_dist) {
             float cur_reward = ctx.data().hiderTeamReward.load_relaxed();
             ctx.data().hiderTeamReward.store_relaxed(cur_reward - 1.f);
-            break;
+            // break;
         }
     }
 }
